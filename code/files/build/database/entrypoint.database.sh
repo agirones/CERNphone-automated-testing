356deb26331d3b225cdff3417e883931ba7a4c60
Andreu Girones de la Fuente <andreu.girones.de.la.fuente@cern.ch>
2022-08-05 17:08:49 +0200

    - build/: All the building is now in functional-testing repo

--- code/files/build/database/entrypoint.database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cleanup old data
-rm -rf /opt/output/*
-# Prepare new data
-python3 /root/database.py
-# Make sure data is accessible
-chmod -R 777 /opt/
--- code/files/build/prepare/entrypoint.prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cleanup old data
-rm -rf /opt/output/*
-# Prepare new data
-python3 /root/prepare.py
-# Make sure data is accessible
-chmod -R 777 /opt/
