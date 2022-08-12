9817aff34100461c73d8211285863eccbeb6f462
Andreu Girones de la Fuente <andreu.girones.de.la.fuente@cern.ch>
2022-08-12 10:26:29 +0200

    after running the cron job all the logs older than a month are removed

--- code/files/log.sh
+++ code/files/log.sh
@@ -3,4 +3,5 @@
 FILE=$(date +\%y-\%m-\%d_\%H:\%M)
 /root/run.sh > /home/agirones/reports/$FILE.log 2>/dev/null
 cp /root/tmp/output/result.jsonl /home/agirones/reports/$FILE.jsonl
-rm $(ls -tr | sed -n 1,2p)
+
+rm $(find . -mtime +30)
