1792d2ee45e3748d7ff038480ca24d8dbfaf8ac7
Andreu Girones de la Fuente <andreu.girones.de.la.fuente@cern.ch>
2022-08-12 09:55:46 +0200

    No printing debug messages when running tests

--- code/files/log.sh
+++ code/files/log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FILE=$(date +\%y-\%m-\%d_\%H:\%M)
-/root/run.sh > /home/agirones/reports/$FILE.log
+/root/run.sh > /home/agirones/reports/$FILE.log 2>/dev/null
 cp /root/tmp/output/result.jsonl /home/agirones/reports/$FILE.jsonl
 rm $(ls -tr | sed -n 1,2p)
